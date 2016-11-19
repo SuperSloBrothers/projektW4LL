16284149e4f35d3935114c765f9e7a870f7345d2
Amir Saifi <PaulMaalouf@Pauls-MacBook-Pro.local>
2016-11-19 02:08:55 -0800

    pods installed

---
+++ projektWALL/projektWALL/APIRouter.swift
@@ -0,0 +1,12 @@
+//
+//  APIRouter.swift
+//  projektWALL
+//
+//  Created by Paul Maalouf on 11/19/16.
+//  Copyright © 2016 slobros. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
