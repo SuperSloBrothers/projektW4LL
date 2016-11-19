9555af3b74ffb3418c454b0c0a4809578e681ab0
Amir Saifi <amirsaifi77@gmail.com>
2016-11-19 13:12:53 -0800

    pods: ReSwift, Dwifft, ObjectMapper

--- projektWALL/Pods/Target Support Files/Pods-projektWALL/Pods-projektWALL-frameworks.sh
+++ projektWALL/Pods/Target Support Files/Pods-projektWALL/Pods-projektWALL-frameworks.sh
@@ -85,7 +85,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Dwifft/Dwifft.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ObjectMapper/ObjectMapper.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ReSwift/ReSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Dwifft/Dwifft.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ObjectMapper/ObjectMapper.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/ReSwift/ReSwift.framework"
 fi
---
+++ projektWALL/projektWALL/RootState.swift
@@ -0,0 +1,14 @@
+//
+//  RootState.swift
+//  projektWALL
+//
+//  Created by Amir Saifi on 11/19/16.
+//  Copyright © 2016 slobros. All rights reserved.
+//
+
+import Foundation
+
+struct RootState {
+    var teamMembers: [String]!
+    var currentDate: Date?
+}
