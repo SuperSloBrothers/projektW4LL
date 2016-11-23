8dbf89ede25be6d5daec1973a227e4f7997be5fb
Amir Saifi <amirsaifi77@gmail.com>
2016-11-22 18:04:40 -0800

    cleanup: Merge

--- projektWALL/projektWALL/AmirStateActions.swift
+++ projektWALL/projektWALL/AmirStateActions.swift
@@ -9,6 +9,7 @@
 import Foundation
 import ReSwift
 import Alamofire
+import ObjectMapper
 
 struct SetName: Action {
     let name: String?
@@ -20,4 +21,30 @@ struct SetThings: Action {
 
 struct SetThingsFromOnline: Action {
     let thingsFromOnline: Result<[AnyObject]>?
+}
+
+func getThingsFromOnline() -> Store<RootState>.ActionCreator {
+    return { state, store in
+        
+        let request = Alamofire.request("https://congress.api.sunlightfoundation.com/legislators").responseJSON { response in
+            guard let JSON = response.result.value else {
+                let apiError = APIError(status: response.response?.statusCode ?? 400, type: "invalid", message: response.result.error?.localizedDescription ?? "could not load legislators")
+                store.dispatch(SetLegislators(legislators: Result.failure(apiError)))
+                return
+            }
+            guard response.response?.statusCode == 200 else {
+                let apiError = Mapper<APIError>().map(JSONObject: JSON)
+                store.dispatch(SetLegislators(legislators: Result.failure(apiError!)))
+                return
+            }
+            if let legislators = Mapper<APILegislatorResult>().map(JSONObject: JSON) {
+                store.dispatch(SetLegislators(legislators: Result.success(legislators.results)))
+            }
+            
+        }
+        print(debugPrint(request))
+        
+        return SetLegislators(legislators: nil)
+    }
+    
 }
